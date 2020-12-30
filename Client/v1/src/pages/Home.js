704d154683d4c615fe862794596bedbc46ee8972
Samuel Aditia <samueladitia95@gmail.com>
2020-12-30 23:56:42 +0700

    add react-boostrap, add react-router

---
+++ Client/v1/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button as="input" type="submit" value="Submit" />{" "}
+    </div>
+  );
+}
+
+export default Home;
